f87483c94a12ad0549c48ab9820f59fd724f96f8
Marsh Dsilva <dsilvamarsh@gmail.com>
2022-06-12 23:03:05 +0100

    initial commit

---
+++ src/main/java/com/bank/core/service/customer/ComBankCoreServiceCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.bank.core.service.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBankCoreServiceCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComBankCoreServiceCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/core/service/customer/ComBankCoreServiceCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.core.service.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComBankCoreServiceCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
